08f7f8cea16b2716e6077ad83e35070f87f2de10
Your Name <your.email@example.com>
2021-02-05 14:30:23 +0900

    コミットメッセージ（自由）

---
+++ database/seeders/TodoTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TodoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $bodys = ['JCBギフト券を換金する', 'ゼルダの伝説をする', 'conohaのVPSで立ち上げをする'];
+        foreach ($bodys as $body) {
+            DB::table('todos')->insert([
+                'body' => $body,
+                'created_at' => new Datetime(),
+                'updated_at' => new Datetime()
+            ]);
+        }
+    }
+}
