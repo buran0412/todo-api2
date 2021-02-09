e76a6c325faa12f456ac30365e35fdd55ff02917
buran0412 <57691797+buran0412@users.noreply.github.com>
2021-02-09 14:56:04 +0900

    Update TodosController.php

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -11,6 +11,28 @@ class TodosController extends Controller
     public function index()
     {
         $todos = Todo::all();
-        return $todos;
+        return redirect('/');
+    }
+    public function store(Request $request)
+    {
+        $todos = new Todo();
+        $todos->body = $request->body;
+        $todos->save();
+        return redirect('/');
+    }
+    public function destroy(todo $todos)
+    {
+        $todos->delete();
+        return redirect('/');
+    }
+    public function edit(todo $todos)
+    {
+        return redirect('/');
+    }
+    public function update(Request $request, todo $todos)
+    {
+        $todos->body = $request->body;
+        $todos->save();
+        return redirect('/');
     }
 }
